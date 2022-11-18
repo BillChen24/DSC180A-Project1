d847e0a39711744c422e381a3f8a5c2eb92a9a23
BillChen24 <bill1317342672@gmail.com>
2022-11-17 20:04:49 -0800

    new data-params

--- src/etl.py
+++ src/etl.py
@@ -29,25 +29,25 @@ import os
 #     return np.load(os.path.join('./data/raw/', test_data))
 # def getTestLabel(test_label):
 #     return np.load(os.path.join('./data/raw/', test_label))
-def getData(train_data, train_label, test_data, test_label):
+def getData(outdir, train_data, train_label, test_data, test_label):
     train_data_ls = []
     for f in train_data:
-        data = np.load(os.path.join('./data/raw/', f))
+        data = np.load(os.path.join(outdir, f))
         train_data_ls.append(data)
 
     train_label_ls = []
     for f in train_label:
-        data = np.load(os.path.join('./data/raw/', f))
+        data = np.load(os.path.join(outdir, f))
         train_label_ls.append(data)
 
     test_data_ls = []
     for f in test_data:
-        data = np.load(os.path.join('./data/raw/', f))
+        data = np.load(os.path.join(outdir, f))
         test_data_ls.append(data)
 
     test_label_ls = []
     for f in test_label:
-        data = np.load(os.path.join('./data/raw/', f))
+        data = np.load(os.path.join(outdir, f))
         test_label_ls.append(data)
 
     return np.concatenate(train_data_ls), np.concatenate(train_label_ls), np.concatenate(test_data_ls), np.concatenate(test_label_ls)
