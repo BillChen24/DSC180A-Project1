11f83ea3d40203b081b9abda133d06ef52d983ee
BillChen24 <bill1317342672@gmail.com>
2022-11-17 16:49:55 -0800

    etl and data params

--- src/etl.py
+++ src/etl.py
@@ -1,10 +1,34 @@
-import pickle
-def get_data(file):
-    with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
-    return dict[b'data']
+# import pickle
+# def get_data(file):
+#     with open(file, 'rb') as fo:
+#         dict = pickle.load(fo, encoding='bytes')
+#     return dict[b'data']
+#
+# def get_label(file):
+#     with open(file, 'rb') as fo:
+#         dict = pickle.load(fo, encoding='bytes')
+#     return dict[b'labels']
 
-def get_label(file):
-    with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
-    return dict[b'labels']
+import numpy as np
+import os
+def getTrainData(train_data):
+    data_ls = []
+    for f in train_data:
+        data = np.load(os.path.join('./data/raw/', f))
+        data_ls.append(data)
+    return np.concatenate(data_ls)
+def getTrainLabel("train_label"):
+    data_ls = []
+    for f in "train_label":
+        data = np.load(os.path.join('./data/raw/', f))
+        data_ls.append(data)
+    return np.concatenate(data_ls)
+
+def getTestData(test_data):
+    return np.load(os.path.join('./data/raw/', test_data))
+def getTestLabel(test_label):
+    return np.load(os.path.join('./data/raw/', test_label))
+def getExampleData():
+    return
+def getExampleLabel():
+    return
