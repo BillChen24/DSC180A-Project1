9804473def6d52238a570e0e41986ead93fec8db
Alina-Zhi <xzhi@ucsd.edu>
2022-12-04 01:58:01 -0800

    update run

--- run.py
+++ run.py
@@ -16,22 +16,23 @@ from src.optimal_transport import *
 
 
 def main(targets):
-	#target=targets.target
+    print(targets)
+    # -------------- DATA LOADING -----------------
     data_to_use=targets[0]
-
+    
     if data_to_use =='test':
         with open('config/test-params.json', 'r') as fh:
             data_params = json.load(fh)
         X_train, y_train, X_test, y_test = getData(**data_params)
         print('load test data')
-        with open('config/model-params.json', 'r') as fh:
-            model_params = json.load(fh)
-        clf = clf_build(X_train, y_train, "RandomForestClassifier")
-        clf_predict(clf, X_test, y_test, "data/out/intial_preds_test.csv")
-        print('initial model on test')
-        X_train_gray = grayscale(X_train, "X_train_gray")
-        X_test_gray = grayscale(X_test, "X_test_gray")
-        print('do gray scale')
+        # with open('config/model-params.json', 'r') as fh:
+        #     model_params = json.load(fh)
+        # clf = clf_build(X_train, y_train, "RandomForestClassifier")
+        # clf_predict(clf, X_test, y_test, "data/out/intial_preds_test.csv")
+        # print('initial model on test')
+        # X_train_gray = grayscale(X_train, "X_train_gray")
+        # X_test_gray = grayscale(X_test, "X_test_gray")
+        # print('do gray scale')
         
 
     elif data_to_use == 'all':
@@ -40,13 +41,20 @@ def main(targets):
             data_params = json.load(fh)
         X_train, y_train, X_test, y_test = getData(**data_params)
         print('load all data')
-
-    #elif data_to_use == 'clean':
-	#TODO: clean all the generate result files
-
-    #    print('clear to clean repo')
-    #else:
-    #    print('No clear instruction')
+        
+        
+    # Train / Read Model:
+    model = train_model( X_train, y_train, X_test, y_test ,targets = targets)    
+    
+    #filter, transform
+    data_paths=filte_transport_data(X_test, y_test)
+    
+    #make predction and get prediction tables
+    create_pred_table(data_paths, model, y_test)
+        
+        
+        
+        
 
     #train classifier on training data, predict on testing data and write prediction
     if 'model' in targets:
@@ -76,7 +84,105 @@ def main(targets):
     if 'test2' in targets:
         clf_predict(clf, X_test_ot, y_test, "data/out/ot_preds.csv")
 
+def train_model(train_data, train_label,test_data, test_label,targets = None):
+    '''
+    detect if word tune in targest, if so, tune model
+    if not, read in best model saved / train default one
+    '''
+    if 'tune' in targets:
+        model = classifier.model_selelection_RandomForestClassifier(train_data, train_label, 
+                                                 depths=[10, 20, 30], 
+                                                 estimators=[100],
+                                                 min_samples_leaf = [5,10,20,40, 50])
+        
+    else:
+    # Read Model
+        model=clf_build(train_data, train_label)
+    # Produce report by class
+    print('Performance on Training Set')
+    clf_predict(model,train_data, train_label, 'result/prediction1.csv')
+    #clf_predict(model,test_data, test_label, 'result/prediction1.csv')
+    # prediction on filtered data, save the result to result
+    prediction_df(train_data, train_label, 
+                         model = model,
+                        outdf_path = 'result/prediction_original.csv')
+    print('Prediction Table saved at', 'result/prediction_original.csv')
+    return model
 
+
+def filte_transport_data(X, y):
+    #return path to the folder containing the filter, trans_without, trains_with class
+    #DO NOT CHANGE KEY NAME!
+    return_dic={'filtered':'data/temp/filtered/filtered.npy',
+               'trans_without_class': 'data/temp/transformer_without_class/transformed.npy',
+               'trans_with_class': [f'data/temp/transformer_with_class/transformed_{i}.npy' for i in [2,4,5,6,7]],
+               'trans_within_class': [f'data/temp/transformer_within_class/transformed_{i}.npy' for i in [2,4,5,6,7]]}
+    if os.path.exists(return_dic['trans_without_class']):
+        return return_dic
+    else: #if no data exist, has to filter, train transporter, and transport data
+        ...
+                                                           
+
+def create_pred_table(data_fp_dict, model, test_label, result_fp = 'result/'):
+    '''
+    data_fp_dict: dictionary contain keys: filtered, trans_without_class, trans_with_class, trans_within_class
+    takes in pretrained model
+    '''
+    return_dict=data_fp_dict.copy()
+    #Filter WithOUT transformer
+#     if data_fp_dict['filtered'] is not None:
+#         fp=data_fp_dict['filtered']
+#         test_data=np.load(fp)
+#         fp=fp.split('/')
+#         outdf_path = 'result/'+fp[-2]+'_'+fp[-1].replace('npy', 'csv')
+#         prediction_df(test_data, test_label, 
+#                          model = model,
+#                         outdf_path = outdf_path)
+#         return_dict['filtered'] = outdf_path
+#         print('prediction table save to', outdf_path)
+    
+    
+    #WITHOUT class Transformed
+    if data_fp_dict['trans_without_class'] is not None:
+        fp=data_fp_dict['trans_without_class']
+        test_data=np.load(fp)
+        fp=fp.split('/')
+        outdf_path = 'result/'+fp[-2]+'_'+fp[-1].replace('npy', 'csv')
+        prediction_df(test_data, test_label, 
+                         model = model,
+                        outdf_path = outdf_path)
+        return_dict['trans_without_class'] = outdf_path
+        print('prediction table save to', outdf_path)
+        
+    #WITH CLASS TRANSFORMED
+    if data_fp_dict['trans_with_class'] is not None:
+        for fp_idx in range(len(data_fp_dict['trans_with_class'])):
+            fp = data_fp_dict['trans_with_class'][fp_idx]
+            test_data=np.load(fp)
+            fp=fp.split('/')
+            outdf_path = 'result/'+fp[-2]+'_'+fp[-1].replace('npy', 'csv')
+            prediction_df(test_data, test_label, 
+                             model = model,
+                            outdf_path = outdf_path)
+            return_dict['trans_with_class'][fp_idx] = outdf_path
+            print('prediction table save to', outdf_path)
+    #WITH CLASS TRANSFORMED
+    if data_fp_dict['trans_within_class'] is not None:
+        for fp_idx in range(len(data_fp_dict['trans_within_class'])):
+            fp = data_fp_dict['trans_within_class'][fp_idx]
+            test_data=np.load(fp)
+            fp=fp.split('/')
+            outdf_path = 'result/'+fp[-2]+'_'+fp[-1].replace('npy', 'csv')
+            prediction_df(test_data, test_label, 
+                             model = model,
+                            outdf_path = outdf_path)
+            return_dict['trans_within_class'][fp_idx] = outdf_path
+            print('prediction table save to', outdf_path)
+        
+    return return_dict
+    
+    
+    
 # # RUN Way1
 #import argparse
 #parser = argparse.ArgumentParser(description = 'DSC180A Project')
@@ -87,3 +193,4 @@ def main(targets):
 if __name__ == '__main__': #if run from command line
     targets = sys.argv[1:]
     main(targets)
+    
