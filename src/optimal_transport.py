ef7a14759a392a70e31ee4b551331ba7e209bbb6
BillChen24 <bill1317342672@gmail.com>
2022-12-03 18:33:03 -0800

    transformed data

--- src/optimal_transport.py
+++ src/optimal_transport.py
@@ -28,6 +28,34 @@ def sample_color_with_class(X_train, y_train, X_gray, n, class_label):
     
     return target_sample, source_sample
 
+def sample_color_within_class(X_train, y_train, X_gray, n, class_label):
+    X_target = X_train[y_train == class_label]
+    X_gray = X_gray[y_train == class_label]
+    
+    X_target_r_pool = np.array([x[:1024] for x in X_target]).flatten()
+    X_target_g_pool = np.array([x[1024:2048] for x in X_target]).flatten()
+    X_target_b_pool = np.array([x[2048:] for x in X_target]).flatten()
+    
+    X_source_r_pool = np.array([x[:1024] for x in X_gray]).flatten()
+    X_source_g_pool = np.array([x[1024:2048] for x in X_gray]).flatten()
+    X_source_b_pool = np.array([x[2048:] for x in X_gray]).flatten()
+    
+    sample1 = np.random.randint(len(X_target_r_pool), size = n)
+    sample2 = np.random.randint(len(X_source_r_pool), size = n)
+    
+    X_target_r = X_target_r_pool[sample1]
+    X_target_g = X_target_g_pool[sample1]
+    X_target_b = X_target_b_pool[sample1]
+    
+    X_source_r = X_source_r_pool[sample1]
+    X_source_g = X_source_g_pool[sample1]
+    X_source_b = X_source_b_pool[sample1]
+    
+    target_sample = np.array([[r, g, b] for r,g,b in zip(X_target_r, X_target_g, X_target_b)])
+    source_sample = np.array([[r, g, b] for r,g,b in zip(X_source_r, X_source_g, X_source_b)])
+    
+    return target_sample, source_sample
+
 def color_ot_build(X_source, X_target, ot_type = ot.da.EMDTransport()):
     ot_type.fit(Xs=X_source, Xt=X_target)
     return ot_type
@@ -42,7 +70,31 @@ def color_ot_transform(X_test_gray, ot_type):
     transformed = np.array([x if x > 0 else 0 for x in transformed])
     return transformed
 
-    
+def color_ot_transform_with_allclass(X_train, y_train, X_train_gray, X_test_gray, n):
+    out = {}
+    for label in set(y_train):
+        print(label)
+        target_sample, source_sample = sample_color_with_class(X_train, y_train, X_train_gray, n, label)
+        ot_transformer = color_ot_build(source_sample, target_sample)
+        transformed_ls = []
+        for x in X_test_gray:
+            transformed = color_ot_transform(x, ot_transformer)
+            transformed_ls.append(transformed)
+        out[label] = np.array(transformed_ls)
+    return out
+
+def color_ot_transform_within_allclass(X_train, y_train, X_train_gray, X_test_gray, n):
+    out = {}
+    for label in set(y_train):
+        print(label)
+        target_sample, source_sample = sample_color_within_class(X_train, y_train, X_train_gray, n, label)
+        ot_transformer = color_ot_build(source_sample, target_sample)
+        transformed_ls = []
+        for x in X_test_gray:
+            transformed = color_ot_transform(x, ot_transformer)
+            transformed_ls.append(transformed)
+        out[label] = np.array(transformed_ls)
+    return out
 
 
 
